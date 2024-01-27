3a059ea4775496ed550de5e2143e37dcae5addff
Ravindra Maurya <ravindramaurya.developer@gmail.com>
2024-01-28 03:54:50 +0530

    implemented profile updation and conversation deletion with confirmation

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
-            'res.cloudinary.com',
-            'lh3.googleusercontent.com',
-            'avatars.githubusercontent.com'
-        ]
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+        // domains: [
+        //     'res.cloudinary.com',
+        //     'lh3.googleusercontent.com',
+        //     'avatars.githubusercontent.com'
+        // ]
     }
 }
 
